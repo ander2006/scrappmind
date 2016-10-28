4e90a8d4ed6cf6831f4ec8ddc06fc0d923c5f43b
ander <anderson_h4@msn.com>
2016-10-27 21:10:32 -0500

    Conecte a base de datos registraduria

---
+++ CAD/CADAdministrador.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+using CAD;
+using System.Configuration;
+using System.Data.SqlClient;
+
+
+namespace CAD
+{
+    public class CADAdministrador
+    {
+        static string cadena = ConfigurationManager.ConnectionStrings["conSQLServer"].ConnectionString;
+        static SqlConnection con = new SqlConnection(cadena);
+        static SqlCommand cmd;
+
+        public void guardarCampos(DTOadministrador adm)
+        {
+            SqlCommand cmd = new SqlCommand(); // sentencias sql
+            cmd.Connection = con;
+            cmd.CommandText = "prc_Guardarcampos";
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@cedula", adm.Cedula);
+            cmd.Parameters.AddWithValue("@departamento", adm.Departamento);
+            cmd.Parameters.AddWithValue("@municipio", adm.Municipio);
+            cmd.Parameters.AddWithValue("@puesto", adm.Puesto);
+            cmd.Parameters.AddWithValue("@dirpuesto", adm.Dirpuesto);
+            cmd.Parameters.AddWithValue("@fecha", adm.Fecha);
+            cmd.Parameters.AddWithValue("@mesa", adm.Mesa);
+            con.Open();
+
+
+            cmd.ExecuteNonQuery();
+            //cerrar conexiòn
+            con.Close();
+            //
+        }
+    }
+}
